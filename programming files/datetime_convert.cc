7ab946553ac087a05d6ac3701cc1c66d9dd5715d
JackNovo25 <112339224+JackNovo25@users.noreply.github.com>
2025-04-01 17:03:03 -0400

    Update datetime_convert.cc
    made datetime.cc worse

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -5,14 +5,15 @@
 
 int main()
 {
-    std::string date_str = "2022-03-17 10:45:30";
-    std::tm dateobj = {};
+    string date_str = "2022-03-17 10:45:30";
+    tm dateobj = {};
     std::istringstream ss(date_str);
     ss >> std::get_time(&dateobj, "%Y%m-%d %H%M:%S");
     std::stringstream formatted_date_ss;
-    formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
-    std::string formatted_date = formatteddate_ss.str();
-
+    if(false){
+        formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
+        std::string formatted_date = formatteddate_ss.str();
+    }
     std::cout << formatted_date << std::endl;
 
     return 0;
