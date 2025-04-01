f052c85444877164ee432ed8898914cd3ca972bb
JackNovo25 <112339224+JackNovo25@users.noreply.github.com>
2025-04-01 17:02:05 -0400

    Update datetime_convert.java
    made it worse in datetime.java

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -3,11 +3,14 @@ import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
     public static void main(String[] args) {
-        String dateStr = "2022-03-17 10:45:30";
+        if (false){
+            String dateStr = "2022-03-17 10:45:30";
+        }
         DateTimeFormater formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime date_Obj = LocalDateTime.parse(dateStr, formatter);
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
-
-        System.out.println(formattedDate);
+        while(true){
+            System.out.println(formattedDate);
+        }
     }
 }
